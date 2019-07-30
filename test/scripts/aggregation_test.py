119607b3462501a938b428ac37bb1b2aaab72d29
Andrei <andrei.perepiolkin@open-e.com>
2019-07-30 16:38:05 +0300

    [~] Improve code style

--- test/scripts/aggregation_test.py
+++ test/scripts/aggregation_test.py
@@ -26,18 +26,18 @@ def clean_vm(root):
 
 
 
-def init_vm(vmName, root):
+def init_vm(name, root):
     """Init vagrant VM in given path"""
-    buildPath = root + "/build"
+    build_path = root + "/build"
     v = vagrant.Vagrant(root=root)
 
     if not os.path.exists(root):
         os.makedirs(root)
 
     print(" - Setting up VM ", root)
-    if not os.path.exists(buildPath):
-        os.makedirs(buildPath)
-    v.init(box_name=vmName)
+    if not os.path.exists(build_path):
+        os.makedirs(build_path)
+    v.init(box_name=name)
 
 def run_vm(root):
     """Start vagrant VM"""
@@ -48,11 +48,11 @@ def run_vm(root):
 
 def init_test_env(src, root):
     """Create necessary resources in folder associated with test"""
-    
+
     build = root + "/build"
     if not os.path.exists(build):
         os.makedirs(build)
-    
+
     try:
         shutil.rmtree(root + "/build/src")
     except FileNotFoundError:
@@ -62,6 +62,8 @@ def init_test_env(src, root):
     os.rename(src, dst)
 
 def load_modules(root):
+    """Insert necessary kernel modules"""
+
     print(" - Loading modules.")
     v = vagrant.Vagrant(root=root)
     cmd = "modprobe iscsi_tcp"
@@ -69,9 +71,11 @@ def load_modules(root):
                      connect_kwargs={
                          "key_filename": v.keyfile(),
                      })
-    out = con.sudo(cmd)
+    con.sudo(cmd)
 
 def register_container_in_vm(root):
+    """Export fresh container to kubernetes cluster"""
+
     print(" - Adding container to the registry.")
     v = vagrant.Vagrant(root=root)
     cmd = "docker load < ./build/src/_output/joviandss-kubernetes-csi-latest"
@@ -79,9 +83,11 @@ def register_container_in_vm(root):
                      connect_kwargs={
                          "key_filename": v.keyfile(),
                      })
-    out = con.sudo(cmd)
+    con.sudo(cmd)
 
 def add_secrets(root):
+    """Add plugin configs as secrets to kubernetes"""
+
     print(" - Adding configs as secrets.")
     v = vagrant.Vagrant(root=root)
     add_controller_cmd = ("kubectl create secret generic jdss-controller-cfg "
@@ -92,10 +98,12 @@ def add_secrets(root):
                      connect_kwargs={
                          "key_filename": v.keyfile(),
                      })
-    out = con.run(add_controller_cmd)
-    out = con.run(add_node_cmd)
+    con.run(add_controller_cmd)
+    con.run(add_node_cmd)
 
 def start_plugin(root):
+    """Start controller and node plugins"""
+
     print(" - Starting plugin.")
     v = vagrant.Vagrant(root=root)
 
@@ -104,22 +112,20 @@ def start_plugin(root):
     node = "./build/src/deploy/joviandss/joviandss-csi-node.yaml"
 
     replace = "sed -i 's/imagePullPolicy: Always/imagePullPolicy:  IfNotPresent/g' "
-    replaceControllerPull = replace + " ./build/src/deploy/joviandss/joviandss-csi-controller.yaml"
-    replaceNodePull = replace +       " ./build/src/deploy/joviandss/joviandss-csi-node.yaml"
 
-    addControllerCmd = "kubectl apply -f " + ctrl
-    addNodeCmd = "kubectl apply -f " + node
+    kub_apply = "kubectl apply -f "
 
     con = Connection(v.user_hostname_port(),
                      connect_kwargs={
                          "key_filename": v.keyfile(),
                      })
-    out = con.run(replaceControllerPull)
-    out = con.run(replaceNodePull)
-    out = con.run(addControllerCmd)
-    out = con.run(addNodeCmd)
+    con.run(replace + ctrl)
+    con.run(replace + node)
+    con.run(kub_apply + ctrl)
+    con.run(kub_apply + node)
 
 def start_nginx(root):
+    """Start nginx plugin with added persistent volume"""
     print(" - Starting test deployment.")
     v = vagrant.Vagrant(root=root)
 
@@ -127,13 +133,17 @@ def start_nginx(root):
                      connect_kwargs={
                          "key_filename": v.keyfile(),
                      })
-    createPVC = "kubectl apply -f ./build/src/deploy/example/nginx-pvc.yaml"
+    create_pvc = "kubectl apply -f ./build/src/deploy/example/nginx-pvc.yaml"
     start_nginx_cmd = "kubectl apply -f ./build/src/deploy/example/nginx.yaml"
-    out = con.run(createPVC)
-    out = con.run(start_nginx_cmd)
+    con.run(create_pvc)
+    con.run(start_nginx_cmd)
 
 
-def wait_for_plugin_started(root, t):
+def wait_for_plugin_started(root, sec):
+    """Wait for controller and node to start
+        by scanning list of kubernetes pods
+    """
+
     print(" - Waiting for plugin to start.")
     v = vagrant.Vagrant(root=root)
 
@@ -142,58 +152,65 @@ def wait_for_plugin_started(root, t):
                          "key_filename": v.keyfile(),
                      })
 
-    controllerRunning = re.compile(r'^joviandss-csi-controller-0.*3\/3.*Running.*$')
-    controllerCreating = re.compile(r'^joviandss-csi-controller-0.*ContainerCreating.*$')
-    node_running = re.compile(r'^joviandss-csi-node-.*2\/2.*Running.*$')
-    node_creating = re.compile(r'^joviandss-csi-node-.*ContainerCreating.*$')
+    ctrl_running_pattern = re.compile(
+        r'^joviandss-csi-controller-0.*3\/3.*Running.*$')
+    ctrl_creating_pattern = re.compile(
+        r'^joviandss-csi-controller-0.*ContainerCreating.*$')
+    node_running_pattern = re.compile(
+        r'^joviandss-csi-node-.*2\/2.*Running.*$')
+    node_creating_pattern = re.compile(
+        r'^joviandss-csi-node-.*ContainerCreating.*$')
 
-    while t > 0:
-        t = t - 1
+    while sec > 0:
+        sec = sec - 1
         time.sleep(1)
         out = str(con.run("kubectl get pods", hide=True).stdout)
 
-        #if len(out):
-        #    continue
+        if not out:
+            continue
 
-        #print("Out: ", out)
-        cr = ""
-        nr = ""
+        ctrl_running = ""
+        node_running = ""
 
         for line in out.splitlines():
-            cr = controllerRunning.search(line)
-            if cr is None:
+            ctrl_running = ctrl_running_pattern.search(line)
+            if ctrl_running is None:
                 continue
             break
 
         for line in out.splitlines():
-            nr = node_running.search(line)
-            if nr is None:
+            node_running = node_running_pattern.search(line)
+            if node_running is None:
                 continue
             break
 
-        if cr != None and nr != None:
+        if ctrl_running != None and node_running != None:
             return True
-        cc = ""
-        nc = ""
+
+        ctrl_creating = ""
+        node_creating = ""
         for line in out.splitlines():
-            cc = controllerCreating.search(line)
-            if cc is None:
-                continue
-            break
-        for line in out.splitlines():
-            nc = node_creating.search(line)
-            if nc is None:
+            ctrl_creating = ctrl_creating_pattern.search(line)
+            if ctrl_creating is None:
                 continue
             break
 
-        if len([i for i in [cc, cr, nc, nr] if i != None]) != 2:
-            print([cc, cr, nc, nr])
+        for line in out.splitlines():
+            node_creating = node_creating_pattern.search(line)
+            if node_creating is None:
+                continue
+            break
+
+        if len([i for i in
+                [ctrl_creating, ctrl_running, node_running, node_running] if i != None]
+              ) != 2:
+            print([ctrl_creating, ctrl_running, node_creating, node_running])
             out = con.run("kubectl get events")
             raise Exception("Fail during plugin loading.")
 
     raise Exception("Unable to get plugins to start running in time.")
 
-def wait_for_nginx_started(root, t):
+def wait_for_nginx_started(root, sec):
     """Startn nginx container with JovianDSS volume
             and wait till it successfully loaded.
     """
@@ -208,36 +225,35 @@ def wait_for_nginx_started(root, t):
     nginx_running = re.compile(r'^nginx.*Running.*$')
     nginx_creating = re.compile(r'^nginx.*ContainerCreating.*$')
 
-    while t > 0:
+    while sec > 0:
         time.sleep(1)
-        t = t - 1
+        sec = sec - 1
         out = str(con.run("kubectl get pods", hide=True).stdout)
 
-        if len(out) == 0:
+        if not out:
             continue
-        nr = ""
 
         for line in out.splitlines():
-            nr = nginx_running.search(line)
-            if nr is None:
+            found = nginx_running.search(line)
+            if found is None:
                 continue
             return True
 
-        np = None
+        pending = None
         for line in out.splitlines():
-            np = nginx_pending.search(line)
-            if np is None:
+            pending = nginx_pending.search(line)
+            if pending is None:
                 continue
             break
 
-        nc = None
+        creating = None
         for line in out.splitlines():
-            nc = nginx_creating.search(line)
-            if nc is None:
+            creating = nginx_creating.search(line)
+            if creating is None:
                 continue
             break
 
-        if (nc is None) and (np is None):
+        if (creating is None) and (pending is None):
             print(out)
             out = con.run("kubectl get events")
             raise Exception("Fail during nginx loading.")
@@ -274,7 +290,7 @@ def main():
     clean_vm(root)
 
     init_vm(csi_test_vm, root)
-    #init_test_env("./build/src", root)
+    init_test_env("./build/src", root)
     try:
         run_vm(root)
         load_modules(root)
